fb8998d63ac8f8c7af92cdac9d1badb7beca1f38
Chanho Kim <cksgh063@gmail.com>
2020-05-09 22:49:39 +0900

    init

---
+++ two_pointer.py
@@ -0,0 +1,18 @@
+n, m = 5, 5
+data = [1, 2, 3, 2, 5]
+
+result = 0
+summary = 0
+end = 0
+
+for start in range(n):
+
+    while summary < m and end < n:
+        summary += data[end]
+        end += 1
+
+    if summary == m:
+        result += 1
+    summary -= data[start]
+
+print(result)
