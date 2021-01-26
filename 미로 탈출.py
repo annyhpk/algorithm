03a4c0d61bc6c3c5c75e68bda743df3f6af339d9
Chanho Kim <cksgh063@gmail.com>
2021-01-27 01:53:28 +0900

    solved

--- 미로 탈출.py
+++ 미로 탈출.py
@@ -11,21 +11,28 @@ board = [
   [1,1,1,1,1,1]
 ]
 
+# board = [
+#   [1,0,1,0,1,0],
+#   [1,1,1,0,1,1],
+#   [0,0,1,1,0,1],
+#   [1,1,1,1,1,1],
+#   [1,1,1,1,1,1]
+# ]
+
 def bfs():
   loc = [0, 0]
-  count = 1
+  count = -1
 
   while loc != [4, 5]:
-
     for i, node in enumerate(board[loc[0]]):
-      if loc[1] <= i:
+      if loc[1] <= i and node != 0 and loc[0]+1 < x:
         count += 1
-        if loc[0]+1 < x: 
-          if node == 1 and board[loc[0]+1][i] == 1:
-            print(loc)
-            loc[0] += 1
-            loc[1] = i
-            break
+        if node == 1 and board[loc[0]+1][i] == 1:  
+          loc[1] = i
+    loc[0] += 1
+    count += 1
+    print(loc)
+
     
   print(count)
 
